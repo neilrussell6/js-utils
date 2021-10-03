70f1f083051fc2d7e7dc38f7875a0987a29796c0
Neil Russell <neilrussell6@gmail.com>
2021-10-03 18:06:58 +0200

    feat(array): export custom errors and constants

--- packages/array/src/index.js
+++ packages/array/src/index.js
@@ -1,1 +1,3 @@
 export { matchSequence, matchSequenceThrow } from './utils'
+export { MatchSequencePartialMatchError, MatchSequenceMismatchError } from './errors'
+export { MATCH_SEQUENCE_RESULT_TYPE } from './constants'
